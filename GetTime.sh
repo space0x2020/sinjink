7b00889a8a177632f95bfd2c5bb89d8255b26df2
Farandole <yshima@lento.sakura.ne.jp>
2021-05-17 09:20:50 +0900

    1. database/sql_select_3.html: SQL文間違い修正 2. database/sql_select_3.html: 職場→部署 3. database/sql_select_2.html: SQL文')'抜け対応 4. database/sql_select_1.html: or -> OR 5. database/index.html: 演習５をsubqの後へ 6. GetTime.sh: stylesamples/のサブディレクトリ除外 7  styleorig/style_svg.css: 誤り修正(':' -> ';')

--- GetTime.sh
+++ GetTime.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gettimef --ignore=.git --ignore=.timestamps.txt --ignore='cpbase/HTML' --ignore='cpbase/MOODLE' --ignore='cpbase/ENCODED' --ignore='database/HTML' --ignore='database/MOODLE' --ignore='svgsamples/HTML' --ignore='svgsamples/MOODLE' --ignore='*.swp' *
+gettimef --ignore=.git --ignore=.timestamps.txt --ignore='cpbase/HTML' --ignore='cpbase/MOODLE' --ignore='cpbase/ENCODED' --ignore='database/HTML' --ignore='database/MOODLE' --ignore='svgsamples/HTML' --ignore='svgsamples/MOODLE' --ignore='svgsamples/ENCODED' --ignore='stylesamples/HTML' --ignore='stylesamples/MOODLE' --ignore='stylesamples/ENCODED' --ignore='*.swp' *
